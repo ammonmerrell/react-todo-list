f00724026a98a3c27633ce86b7c8474ea192636d
ammonmerrell <ammonmerrell@outlook.com>
2026-03-04 09:49:21 -0700

    added check and delete functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import "./App.css"
 export default function App () {
   const [newItem, setNewItem] = useState("")
   const [todos, setTodos] = useState([])
-  
+
   function handleSubmit(e) {
     e.preventDefault()
   
@@ -17,7 +17,25 @@ export default function App () {
         false },
     ]
   })
+  setNewItem("")
 }
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id ===id){
+          return {...todo, completed}
+        }
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id){
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
 
   return (
   <>
@@ -31,21 +49,22 @@ export default function App () {
   </form>
   <h1 className="header">Todo List</h1>
   <ul className="list">
-    <li>
+    {todos.length === 0 && "No Todos"}
+
+    {todos.map(todo =>{
+      return (
+      <li key={todo.id}>
       <label>
-        <input type="checkbox" />
-        Item 1
+        <input type="checkbox" checked={todo.completed}
+        onChange={e => toggleTodo(todo.id, e.target.checked)}/>
+        {todo.title}
       </label>
-      <button className="btn btn-danger">Delete</button>
+      <button onClick={() =>deleteTodo(todo.id)}
+      className="btn btn-danger">Delete</button>
     </li>
-    <li>
-      <label>
-        <input type="checkbox" />
-        Item 2
-      </label>
-      <button className="btn btn-danger">Delete</button>
-    </li>
-  </ul>
+    )
+    })}
+    </ul>
   </>
   )
 }
